52f8a65af4d9b625d41fa53fbe8d443b2173428a
Vivek Panyam <viv.panyam@gmail.com>
2023-02-13 02:05:07 -0500

    Move zip extraction into `carton-utils` crate

--- source/carton-runner-py/src/wheel.rs
+++ source/carton-runner-py/src/wheel.rs
@@ -2,6 +2,7 @@ use std::path::{Path, PathBuf};
 
 use async_zip::read::fs::ZipFileReader;
 use carton_runner_interface::slowlog::{slowlog, Progress};
+use carton_utils::archive::{extract_zip, with_atomic_extraction};
 use lazy_static::lazy_static;
 use path_clean::PathClean;
 use sha2::{Digest, Sha256};
@@ -33,7 +34,7 @@ pub async fn install_wheel(url: &str, sha256: &str) -> PathBuf {
     let target_dir = PACKAGE_BASE_DIR.join(sha256);
     if target_dir.exists() {
         // This already exists
-        // TODO: we should probably do some locking to avoid wasted parallel installations
+        // TODO: we should probably also do some locking to avoid wasted parallel installations
         return target_dir;
     }
 
@@ -79,90 +80,12 @@ pub async fn install_wheel(url: &str, sha256: &str) -> PathBuf {
         .without_progress();
 
     // Unzip
-    let extraction_dir = tempdir.path().join("extraction");
-    unzip_file(&download_path, &extraction_dir).await;
+    with_atomic_extraction(&download_path, &target_dir, extract_zip).await;
 
     sl.done();
 
-    // Move to the target directory. This should be atomic so it won't break anything
-    // if multiple installs happen at the same time.
-    match tokio::fs::rename(extraction_dir, &target_dir).await {
-        Err(e) if e.raw_os_error() == Some(libc::ENOTEMPTY) => {
-            // This can happen if another installation created the target directory before we called
-            // rename.
-            // We don't need to do anything here
-        }
-        e => e.unwrap(),
-    }
-
     // Return the path to add to sys.path
     target_dir
-}
-
-// Based on https://github.com/Majored/rs-async-zip/blob/main/examples/file_extraction.rs
-/// Extracts everything from the ZIP archive to the output directory
-pub(crate) async fn unzip_file(archive: &Path, out_dir: &Path) {
-    let mut handles = Vec::new();
-    let reader = ZipFileReader::new(archive)
-        .await
-        .expect("Failed to read zip file");
-    for index in 0..reader.file().entries().len() {
-        let entry = &reader.file().entries().get(index).unwrap().entry();
-
-        // Normalize the file path
-        let path = out_dir.join(entry.filename()).clean();
-
-        // Ensure that path is within the base dir
-        if !path.starts_with(out_dir) {
-            panic!("Error: extracted file path does not start with the output dir")
-        }
-
-        // If the filename of the entry ends with '/', it is treated as a directory.
-        // This is implemented by previous versions of this crate and the Python Standard Library.
-        // https://docs.rs/async_zip/0.0.8/src/async_zip/read/mod.rs.html#63-65
-        // https://github.com/python/cpython/blob/820ef62833bd2d84a141adedd9a05998595d6b6d/Lib/zipfile.py#L528
-        let entry_is_dir = entry.filename().ends_with('/');
-
-        if entry_is_dir {
-            // The directory may have been created if iteration is out of order.
-            if !path.exists() {
-                tokio::fs::create_dir_all(&path)
-                    .await
-                    .expect("Failed to create extracted directory");
-            }
-        } else {
-            // Creates parent directories. They may not exist if iteration is out of order
-            // or the archive does not contain directory entries.
-            let parent = path
-                .parent()
-                .expect("A file entry should have parent directories");
-            if !parent.is_dir() {
-                tokio::fs::create_dir_all(parent)
-                    .await
-                    .expect("Failed to create parent directories");
-            }
-            let mut writer = tokio::fs::OpenOptions::new()
-                .write(true)
-                .create_new(true)
-                .open(&path)
-                .await
-                .expect("Failed to create extracted file");
-
-            // Spawn a task to extract
-            let reader = reader.clone();
-            handles.push(tokio::spawn(async move {
-                let mut entry_reader = reader.entry(index).await.expect("Failed to read ZipEntry");
-                tokio::io::copy(&mut entry_reader, &mut writer)
-                    .await
-                    .expect("Failed to copy to extracted file");
-            }));
-        }
-    }
-
-    // Wait until all the files are extracted
-    for handle in handles {
-        handle.await.unwrap();
-    }
 }
 
 #[cfg(test)]
