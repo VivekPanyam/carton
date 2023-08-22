e126e7b9bd7fa8fc76919828d5a9e2fafe1ae109
Vivek Panyam <viv.panyam@gmail.com>
2023-08-22 19:59:19 -0400

    Move nightly Linux builds to Buildkite

---
+++ ci/trigger_buildkite_build.py
@@ -0,0 +1,78 @@
+# This script triggers a buildkite build for the current SHA and branch, waits for it to complete, and then
+# downloads artifacts. It looks at GITHUB_* env vars and is intended to be run from github actions.
+#
+# To use, make sure requests is installed (pip3 install requests)
+import os
+import requests
+import time
+
+
+BUILDKITE_TOKEN = os.getenv("BUILDKITE_TOKEN")
+if BUILDKITE_TOKEN is None:
+    raise ValueError("Expected BUILDKITE_TOKEN env var to be set")
+
+GITHUB_SHA = os.getenv("GITHUB_SHA")
+GITHUB_REF_TYPE = os.getenv("GITHUB_REF_TYPE")
+
+if GITHUB_REF_TYPE != "branch":
+    raise ValueError("Expected GITHUB_REF_TYPE to be 'branch'")
+
+GITHUB_BRANCH = os.getenv("GITHUB_REF_NAME")
+
+# Trigger a build
+
+res = requests.post("https://api.buildkite.com/v2/organizations/carton/pipelines/nightly/builds", headers={"Authorization": f"Bearer {BUILDKITE_TOKEN}"}, json={
+    "commit": GITHUB_SHA,
+    "branch": GITHUB_BRANCH,
+    "message": "Nightly build",
+})
+
+data = res.json()
+build_url = data["url"]
+build_number = data["number"]
+
+print(f"Created build {data['web_url']}")
+
+while True:
+    print("Waiting for build to complete...")
+
+    # Get the build status
+    data = requests.get(build_url, headers={"Authorization": f"Bearer {BUILDKITE_TOKEN}"}).json()
+    state = data["state"]
+
+    if state == "passed":
+        break
+
+    # States that are okay, but we need to keep checking
+    if state in ["creating", "scheduled", "running"]:
+        # Sleep for a bit before rechecking
+        time.sleep(10.0)
+    else:
+        raise ValueError(f"The build failed with status: {state}")
+
+print("Build complete! Getting artifacts...")
+
+fetch_url = f"https://api.buildkite.com/v2/organizations/carton/pipelines/nightly/builds/{build_number}/artifacts"
+while fetch_url is not None:
+    res = requests.get(fetch_url, headers={"Authorization": f"Bearer {BUILDKITE_TOKEN}"})
+
+    # Get the next url if any
+    if 'next' in res.links:
+        fetch_url = res.links['next']['url']
+    else:
+        fetch_url = None
+
+    artifacts = res.json()
+    for artifact in artifacts:
+        download_url = artifact["download_url"]
+        dirname = artifact["dirname"]
+        filename = artifact["filename"]
+        target_dir = os.path.join("/tmp", dirname)
+        os.makedirs(target_dir, exist_ok=True)
+        target_path = os.path.join(target_dir, filename)
+
+        print(f"Downloading {filename} to {target_path} ...")
+
+        res = requests.get(download_url, headers={"Authorization": f"Bearer {BUILDKITE_TOKEN}"})
+        with open(target_path, "wb") as f:
+            f.write(res.content)
