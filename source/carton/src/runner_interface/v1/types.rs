d6e05b691d483e7b2ce42a2f502b3e6c17aa321e
Vivek Panyam <viv.panyam@gmail.com>
2023-09-25 23:14:20 -0400

    License as Apache 2.0 (#144)
    Add a `LICENSE` file and add license headers to source files

--- source/carton/src/runner_interface/v1/types.rs
+++ source/carton/src/runner_interface/v1/types.rs
@@ -1,3 +1,17 @@
+// Copyright 2023 Vivek Panyam
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 //! Implements type conversions between carton::types and carton-runner-interface types for V1 of the runner interface
 
 use crate::conversion_utils::convert_vec;
